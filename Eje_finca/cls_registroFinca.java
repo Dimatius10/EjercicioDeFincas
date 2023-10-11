e2226c4c1fd46a76e9787742267d29d2231d454b
Mateo Barco <mateo.barcomo@amigo.edu.co>
2023-10-11 08:46:06 -0500

    Creación constructor
    Por Mateo Barco

--- Eje_finca/cls_registroFinca.java
+++ Eje_finca/cls_registroFinca.java
@@ -1,10 +1,22 @@
 public class cls_registroFinca {
-    private String nombre;
-    private String codigo;
-    private String extension;
-    private String propietario;
-    private String direccion;
-    private String ubicacion;
-    private String act_economica;
-    private float prom_produccion;
+    private String str_nombre;
+    private String str_codigo;
+    private String str_extension;
+    private String str_propietario;
+    private String str_direccion;
+    private String str_ubicacion;
+    private String str_act_economica;
+    private float flt_prom_produccion;
+
+    public cls_registroFinca(String nombre, String codigo, String extension, String propietario, String direccion, String ubicacion, String act_economica, float prom_produccion){
+        this.str_nombre = nombre;
+        this.str_codigo = codigo;
+        this.str_extension = extension;
+        this.str_propietario = propietario;
+        this.str_direccion = direccion;
+        this.str_ubicacion = ubicacion;
+        this.str_act_economica = act_economica;
+        this.flt_prom_produccion = prom_produccion;
+    }
+    
 }
