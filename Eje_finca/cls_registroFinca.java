de6c5fcb8df2c20f7fd63ba4354672da272383cd
Mateo Barco <mateo.barcomo@amigo.edu.co>
2023-10-11 08:37:27 -0500

    clase del registro de fincas
    Por Mateo Barco

---
+++ Eje_finca/cls_registroFinca.java
@@ -0,0 +1,10 @@
+public class cls_registroFinca {
+    private String nombre;
+    private String codigo;
+    private String extension;
+    private String propietario;
+    private String direccion;
+    private String ubicacion;
+    private String act_economica;
+    private float prom_produccion;
+}
