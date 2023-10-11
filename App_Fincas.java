6e35d919603d9bdf473416b8ccd729ca4de57fe5
EinerQ <einer.quirozpi@amigo.edu.co>
2023-10-11 08:45:48 -0500

    creacion del vector

--- App_Fincas.java
+++ App_Fincas.java
@@ -1,3 +1,6 @@
 public class App_fincas {
-    
+    static cls_registroFinca[] Finca = new cls_registroFinca[20];
+    static int int_posiciones = 0;
+    static boolean bln_sw;
+    static int int_pos;
 }
