dad7a8f70933b99a34ab25104031d744621d28ae
passos2001 <145952610+MartWolf23@users.noreply.github.com>
2023-10-11 08:46:06 -0500

    cls_parcelacion
    creacion de variables

---
+++ Eje_finca/cls_parcelacion.java
@@ -0,0 +1,27 @@
+
+public class cls_parcelacion{
+    String str_codigo ;
+    String str_nombre;
+    String str_actividad;
+    String str_produccion;
+    String str_stock;
+    String str_cod_finca;
+
+
+    public cls_parcelacion(String codigo , String nombre , String actividad , String produccion , String stock, String cod_finca){
+        
+    this.str_codigo = codigo;
+    this.str_nombre = nombre ;
+    this.str_actividad = actividad ;
+    this.str_produccion = produccion ;
+    this.str_stock = stock ;
+    this.str_cod_finca = cod_finca ;
+
+    }
+
+// geters 
+
+
+}
+
+
