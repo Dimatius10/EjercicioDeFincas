396574cba8eb31913105619b7631be12e3bb1c76
EinerQ <einer.quirozpi@amigo.edu.co>
2023-10-11 09:03:08 -0500

    nuevo

--- Eje_finca/App_Fincas.java
+++ Eje_finca/App_Fincas.java
@@ -1,15 +1,22 @@
 import javax.swing.JOptionPane;
 
-public class App_fincas {
+public class App_Fincas {
     static cls_registroFinca[] Finca = new cls_registroFinca[20];
     static int int_posiciones = 0;
     static boolean bln_sw;
     static int int_pos;
 
     public static void main(String[] args) {
-        
+        fnt_menuPrincipal(true);
     }
-    
+    private static void fnt_selectorP(int opcionP){
+        if (opcionP == 1){
+            String nombre = JOptionPane.showInputDialog(null, "Nombre de la finca: ");
+            String codigo = JOptionPane.showInputDialog(null, " Codigo de la finca: ");
+            String extension = JOptionPane.showInputDialog(null,  "Extension: ");
+            String
+        }
+    }
     private static void fnt_menuPrincipal(boolean mp){
         while (mp == true){
             int m = Integer.parseInt(JOptionPane.showInputDialog(null , "<<<MENU PRINCIPAL>>>\n" 
