a3a18ca65bd2920fd8efde88263c00354570b9b7
EinerQ <einer.quirozpi@amigo.edu.co>
2023-10-11 08:54:08 -0500

    creacion del menu principal

--- App_Fincas.java
+++ App_Fincas.java
@@ -1,6 +1,22 @@
+import javax.swing.JOptionPane;
+
 public class App_fincas {
     static cls_registroFinca[] Finca = new cls_registroFinca[20];
     static int int_posiciones = 0;
     static boolean bln_sw;
     static int int_pos;
+
+    public static void main(String[] args) {
+        
+    }
+    
+    private static void fnt_menuPrincipal(boolean mp){
+        while (mp == true){
+            int m = Integer.parseInt(JOptionPane.showInputDialog(null , "<<<MENU PRINCIPAL>>>\n" 
+            + "1.Registro Finca\n" 
+            + "2.Parcelación \n" 
+            + "3.Reportes\n" 
+            + "4.Salir"));
+        }
+    }
 }
